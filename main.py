13f11fbb28a5ff4105a55390bebb288dc975ce89
David Florness <edwargix@gmail.com>
2017-04-11 23:56:49 -0500

    Display each team name next to the number

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.pagelayout import PageLayout
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, StringProperty
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 import os
@@ -14,6 +14,7 @@ kivy.require('1.9.1')
 
 class Team(Widget):
     num = NumericProperty()
+    name = StringProperty()
 
 
 class TeamScreen(Screen):
@@ -25,11 +26,11 @@ class TeamsScreen(Screen):
         numbers = []
         file = open(os.getcwd() + '/teams.txt', 'r')
         for line in file.readlines():
-            split = line.split("\t")
-            numbers.append(int(split[0]))
+            split = line.split("|")
+            numbers.append((int(split[0]), str(split[1])))
         print "Team numbers: ", numbers
-        for num in numbers:
-            self.ids.team_grid.add_widget(Team(num=num))
+        for num, name in numbers:
+            self.ids.team_grid.add_widget(Team(num=num, name=name))
         self.ids.team_grid.rows = len(numbers)
 
 
