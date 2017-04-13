6029c1882b1dddc90cf4d4d45badaabe46f3d4ea
David Florness <edwargix@gmail.com>
2017-04-13 00:57:22 -0500

    Use data stored in 'teams' dictionary

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.pagelayout import PageLayout
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, StringProperty
+from kivy.properties import NumericProperty, StringProperty, DictProperty
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 
@@ -19,54 +19,37 @@ teams = {}
 
 
 class Team(Widget):
-    num = NumericProperty()
+    num = StringProperty()
     name = StringProperty()
 
 
 class TeamScreen(Screen):
-    num = NumericProperty()
+    num = StringProperty()
+    values = DictProperty()
 
 
 class TeamsScreen(Screen):
     def do_layout(self, *args):
-        numbers = []
-        teams_file = open(os.getcwd() + '/teams.txt', 'r')
-        for line in teams_file.readlines():
-            split = line.split("|")
-            numbers.append((int(split[0]), str(split[1])))
-        print "Team numbers: ", numbers
-        self.ids.team_grid.rows = len(numbers)
-        for num, name in numbers:
-            self.ids.team_grid.add_widget(Team(num=num, name=name))
+        self.ids.team_grid.rows = len(teams)
+        for team in sorted(teams.iterkeys()):
+            self.ids.team_grid.add_widget(Team(num=team, name=teams[team]['name']))
 
 
 class ScoutScreenManager(ScreenManager):
     def goto_team(self, n):
-        s = TeamScreen(num=n, name=str(n))
+        s = TeamScreen(num=n, name=str(n), values=teams[str(n)])
         self.add_widget(s)
         self.current = str(n)
 
-    def save_team(self, n):
+    def save_team(self, n, values):
         print 'Saving data for team ', n
-        done = False
-        # Format:
-        # [match numbers],[bool,bool,bool],"Notes",["teleop"]
-        try:
-            with open('data.txt', 'r') as read_data:
-                for line in read_data.readlines():
-                    print 'Line: ', line
-                    split = line.split('|')
-                    if (split[0] == str(n)):
-                        # TODO: change data for team
-                        done = True
-                        break
-            if (done == False):
-                print 'Attempting to append data'
-                with open('data.txt', 'a') as append_data:
-                    append_data.write('\n' + str(n) + '|')
-        except IOError:
-            with open('data.txt', 'w') as data:
-                data.write(str(n) + '|')
+        old_name = teams[n]['name']
+        teams[n] = values
+        teams[n]['name'] = old_name
+
+        with open('data.txt', 'w') as file:
+            file.write(json.dumps(teams))
+            print 'team! ', teams[n]
 
 
 class ScoutApp(App):
